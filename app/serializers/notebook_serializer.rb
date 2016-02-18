134d8dab73ef58b29975b472dca69ac9866cb515
kaylee42 <kayleeknowles42@gmail.com>
2016-02-18 15:48:26 -0500

    removes unnecessary routes

--- app/serializers/notebook_serializer.rb
+++ app/serializers/notebook_serializer.rb
@@ -1,7 +1,7 @@
 class NotebookSerializer < ActiveModel::Serializer
   embed :ids, include: true
   attributes :id, :title, :description
-  has_many :sheets
+  has_many :sheets, serializer: SheetMediumSerializer
   has_many :users, serializer: UserLiteSerializer
   has_many :tags, serializer: TagLiteSerializer
 end
