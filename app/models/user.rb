ecadf00eba2c5252a3162ddb0cfa51045310abb0
kaylee42 <kayleeknowles42@gmail.com>
2016-02-19 08:01:07 -0500

    initializes devise setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :notebook_users
   has_many :notebooks, through: :notebook_users
   has_many :todo_lists
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+root "notebooks#index" 
+
   namespace :api do
     namespace :v1 do
       resources :tags, except: [:new, :edit]
