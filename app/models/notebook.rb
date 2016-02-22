28fe280a36c05a58f3633528f4b8f4135f62744f
jappleba <josh.applebaum@flatironschool.com>
2016-02-22 17:45:15 -0500

    adds update and delete notebook

--- app/models/notebook.rb
+++ app/models/notebook.rb
@@ -1,9 +1,10 @@
 class Notebook < ActiveRecord::Base
-  has_many :notebook_users
+  has_many :notebook_users, dependent: :destroy
   has_many :users, through: :notebook_users
   has_many :notebook_sheets
   has_many :sheets, through: :notebook_sheets
   has_many :tags, through: :sheets
+
 
   validates_presence_of :title
 end
